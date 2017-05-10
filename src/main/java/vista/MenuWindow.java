3c98c5d496d610455682e58e50b348d1db464561
ezeqperez <ezequielperez90@gmail.com>
2017-05-10 19:33:12 -0300

    Solo poner las pantallas que necesitan #2

--- src/main/java/vista/MenuWindow.java
+++ src/main/java/vista/MenuWindow.java
@@ -1,13 +1,12 @@
 package vista;
 
+import modelo.CuentasViewModel;
 import org.uqbar.arena.layout.ColumnLayout;
 import org.uqbar.arena.widgets.Button;
 import org.uqbar.arena.widgets.Label;
 import org.uqbar.arena.widgets.Panel;
 import org.uqbar.arena.windows.SimpleWindow;
 import org.uqbar.arena.windows.WindowOwner;
-
-import modelo.CuentasViewModel;
 import org.uqbar.commons.model.UserException;
 
 @SuppressWarnings("serial")
@@ -22,23 +21,10 @@ public class MenuWindow extends SimpleWindow<CuentasViewModel> {
         this.setTitle("¡Bienvenido!");
         Panel columnasPanel = new Panel(mainPanel);
         columnasPanel.setLayout(new ColumnLayout(2));
-
-        new Label(mainPanel).setText("Menú principal:");
-
+        
         new Button(columnasPanel).setCaption("Cargar cuentas").onClick(() -> this.cargarCuentas());
 
         new Button(columnasPanel).setCaption("Consultar cuentas").onClick(() -> this.buscarCuentas());
-
-        new Button(columnasPanel).setCaption("Crear nuevo indicador").onClick(() -> this.mostrarAlerta("Todavía en construcción :|"));
-
-        new Button(columnasPanel).setCaption("Consultar indicadores").onClick(() -> this.mostrarAlerta("Todavía en construcción :|"));
-
-        new Button(columnasPanel).setCaption("Consultar gráficos").onClick(() -> this.mostrarAlerta("Todavía en construcción :|"));
-
-        new Button(columnasPanel).setCaption("Crear nueva metodología").onClick(() -> this.mostrarAlerta("Todavía en construcción :|"));
-
-        new Button(columnasPanel).setCaption("Nuevo análisis").onClick(() -> this.mostrarAlerta("Todavía en construcción :|"));
-
     }
 
     private void cargarCuentas() {
