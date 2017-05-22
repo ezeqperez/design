bb65d6daf9fb4083531d50b3de36506308a60307
retconadmin <gfrias@garbarino.com.ar>
2017-05-22 15:56:19 -0300

    prueba

--- src/main/java/utils/UploadMock.java
+++ src/main/java/utils/UploadMock.java
@@ -1,6 +1,8 @@
 package utils;
 
 import modelo.dominio.Cuenta;
+import modelo.dominio.Empresa;
+
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
@@ -8,16 +10,16 @@ import java.util.List;
 public class UploadMock implements Uploader {
     List<Cuenta> cuentas = new ArrayList<Cuenta>();
 
-    public List<Cuenta> procesarArchivo(String ruta) {
+    public List<Empresa> procesarArchivo(String ruta) {
         Cuenta cuenta1 = new Cuenta();
-        cuenta1.setEmpresa("Facebook");
+        //cuenta1.setEmpresa("Facebook");
         cuenta1.setMonto(new BigDecimal(999));
-        cuenta1.setPeriodo("2016");
-        cuenta1.setCuenta("ModeloInteresante");
-        List<Cuenta> tresCuentas = new ArrayList<Cuenta>();
-        tresCuentas.add(cuenta1);
-        tresCuentas.add(cuenta1);
-        tresCuentas.add(cuenta1);
+        //cuenta1.setPeriodo("2016");
+        //cuenta1.setCuenta("ModeloInteresante");
+        List<Empresa> tresCuentas = new ArrayList<>();
+        //tresCuentas.add(cuenta1);
+        //tresCuentas.add(cuenta1);
+        //tresCuentas.add(cuenta1);
 
         return tresCuentas;
     }
