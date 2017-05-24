9e18f4e83b2763c315167e9304315b0bdd84a162
retconadmin <gfrias@garbarino.com.ar>
2017-05-24 09:55:18 -0300

    Pruebas para arrancar el parser

--- src/main/java/utils/Indicadores/IndicadoresUpload.java
+++ src/main/java/utils/Indicadores/IndicadoresUpload.java
@@ -1,7 +1,25 @@
 package utils.Indicadores;
 
-/**
- * Created by retconadmin on 22/05/17.
- */
+import java.io.Reader;
+import java.io.StringReader;
+
 public class IndicadoresUpload {
+/*
+    public ArithmeticParser(String s)
+    {
+        this((Reader)(new StringReader(s)));
+
+    }*/
+
+    public void cargarIndicadores(String ruta) {
+        //leer el archivo, asignarlo a un string y correr nuestro parser para instanciar indicadores
+        /*
+        try {
+            String s = FileReader BLABLA;
+            ArithmeticParser parser = new ArithmeticParser(s);
+            parser.parsear();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }*/
+    }
 }
