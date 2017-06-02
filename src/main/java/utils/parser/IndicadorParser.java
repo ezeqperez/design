9d379c8dca193322648bab8d22ad655bb3739e89
ezeqperez <ezequielperez90@gmail.com>
2017-06-02 12:41:04 -0300

    Test de ArithemticParser y varios

--- src/main/java/utils/parser/IndicadorParser.java
+++ src/main/java/utils/parser/IndicadorParser.java
@@ -2,16 +2,10 @@ package utils.parser;
 
 import javacc.ArithmeticParser;
 import javacc.ParseException;
-import javacc.Token;
 import modelo.dominio.Indicador;
-import modelo.dominio.operandos.CuentaOperando;
-import modelo.dominio.operandos.Operando;
-import modelo.dominio.operandos.Valor;
 import org.uqbar.commons.model.UserException;
 
 import java.io.StringReader;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Parser que recibe un string con la informacion de
@@ -20,8 +14,6 @@ import java.util.List;
 public class IndicadorParser {
 
     private Indicador indicador;
-    private List<Token> tokens = new ArrayList<Token>();
-
     public Indicador generarIndicador(String nombre, String formula){
         StringReader sr = new StringReader(formula);
         ArithmeticParser arithmeticParser = new ArithmeticParser(sr);
@@ -31,36 +23,9 @@ public class IndicadorParser {
         }catch(ParseException e){
             throw new UserException("La formula que ingreso no es valida, por favor intente de nuevo");
         }
-/*
         indicador.setNombre(nombre);
         indicador.setFormula(formula);
 
-        //El primerOperador está muy acoplado, recibe un token, no encuentro otra manera
-        indicador.setPrimerOperando(this.obtenerOperando(arithmeticParser.getOperando1()));
-        indicador.setSimbolo(arithmeticParser.getOperacion());
-        indicador.setSegundoOperando(new Valor(arithmeticParser.getOperando2()));
-
-*/
         return indicador;
-    }
-
-    /*
-    * Si es un numero, lo cargo como un Valor.
-    * si llega a ser una cuenta o un indicador, lo voy a guardar como Valor Compuesto,
-    * (ahora llamado CuentaOperando), lo que quiero es para luego calcular, que el valor
-    *  me sepa devolver el mismo valor que tiene (que devuelve mi parser), si llega a ser
-    *  una Cuenta, tiene que devolverme el costo de la misma, y si llega a ser un Indicador,
-    *  debe calcular los valores de todos sus operadores.
-    */
-    private Operando obtenerOperando(Token token) {
-        //Valores numericos
-        if(token.kind == 11){
-            return new Valor(token.toString());
-        //Cuentas
-        }else if(token.kind == 12) {
-            //Debo ver si esta cuenta existe
-            return new CuentaOperando(token.toString());
-        }
-        return null;
     }
 }
